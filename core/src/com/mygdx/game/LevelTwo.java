1479507995dea45f4b50fd57eb35a8792e9b788a
Teemu <teemu.tannerma@cs.tamk.fi>
2018-04-09 00:47:10 +0300

    Finished the second level. Made size changes to tutorial level.

--- core/src/com/mygdx/game/LevelTwo.java
+++ core/src/com/mygdx/game/LevelTwo.java
@@ -39,6 +39,11 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
     boolean blueColorChanged;
     boolean purpleColorChanged;
     boolean secondRedColorChanged;
+    boolean whiteColorChanged;
+    boolean cyanColorChanged;
+    boolean blackColorChanged;
+    boolean yellowColorChanged;
+    boolean secondWhiteChanged;
     boolean redColor;
     boolean blueColor;
     String puddleCol;
@@ -56,6 +61,11 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
         blueColorChanged = false;
         redColorChanged = false;
         purpleColorChanged = false;
+        whiteColorChanged = false;
+        cyanColorChanged = false;
+        blackColorChanged = false;
+        secondWhiteChanged = false;
+
         redColor = false;
         blueColor = false;
     }
@@ -85,8 +95,13 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
         redColorChanged = checkPaintCollision(redColorChanged, "red_puddle_object");
         blueColorChanged = checkPaintCollision(blueColorChanged, "blue_puddle_object");
         secondRedColorChanged = checkPaintCollision(secondRedColorChanged, "second_red_puddle_obj");
+        whiteColorChanged = checkPaintCollision(whiteColorChanged, "white_puddle_obj");
+        blackColorChanged = checkPaintCollision(blackColorChanged, "black_puddle_obj");
+        yellowColorChanged = checkPaintCollision(yellowColorChanged, "yellow_puddle_obj");
+        secondWhiteChanged = checkPaintCollision(secondWhiteChanged, "second_white_puddle_obj");
 
-        setColorOfPlayer(redColorChanged, blueColorChanged, secondRedColorChanged);
+
+        setColorOfPlayer(redColorChanged, blueColorChanged, secondRedColorChanged, whiteColorChanged, blackColorChanged, yellowColorChanged, secondWhiteChanged);
 
         if(checkResetCollision()) {
             changeColor("null");
@@ -94,15 +109,49 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
             player.setRed(false);
             player.setBlue(false);
             player.setSecondRedColor(false);
+            player.setWhite(false);
+            player.setBlack(false);
+            redColorChanged = false;
+            blueColorChanged = false;
+            secondRedColorChanged = false;
+            whiteColorChanged = false;
+            blackColorChanged = false;
+            yellowColorChanged = false;
+        }
+
+        if(checkGoalCollision()) {
+            MapFinished mapFinished = new MapFinished(host);
+            host.setScreen(mapFinished);
         }
 
         batch.end();
 
         player.render(batch);
-
-
-
     }
+
+    private boolean checkGoalCollision() {
+
+        // Gets red gate rectangle layer.
+        MapLayer collisionObjectLayer = tiledMap.getLayers().get("goal_object");
+
+        // All the objects of the layer.
+        MapObjects mapObjects = collisionObjectLayer.getObjects();
+
+        //Collects all rectangles in an array.
+        Array<RectangleMapObject> rectangleObjects = mapObjects.getByType(RectangleMapObject.class);
+
+        // Loop through all rectangles.
+        for(RectangleMapObject rectangleObject : rectangleObjects) {
+            com.badlogic.gdx.math.Rectangle rectangle = rectangleObject.getRectangle();
+
+            if(player.playerRectangle.overlaps(rectangle)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public void setPuddleCol(String puddleCol) {
 
         this.puddleCol = puddleCol;
@@ -113,18 +162,18 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
         return puddleCol;
     }
 
-    private void setColorOfPlayer(boolean red, boolean blue, boolean secondRed) {
-        if(red && !blue) {
+    private void setColorOfPlayer(boolean red, boolean blue, boolean secondRed, boolean white, boolean black, boolean yellow, boolean secondWhite) {
+        if(red && !blue && !yellow && !white && !black) {
             setPuddleCol("red");
             clearGate("red_gate");
             player.setRed(true);
         }
-        if(blue && !red) {
+        if(blue && !red && !white && !black && !yellow) {
             setPuddleCol("blue");
             clearGate("blue_gate");
             player.setRed(true);
         }
-        if(red && blue) {
+        if(red && blue && !white && !black && !yellow) {
             setPuddleCol("purple");
             clearGate("purple_gate");
             clearGate("blue_gate");
@@ -132,7 +181,45 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
         if(secondRed) {
             setPuddleCol("secondRed");
             clearGate("red_gate_second");
+            player.setSecondRedColor(true);
         }
+        if(white && !red && !blue && !black && !yellow) {
+            setPuddleCol("white");
+            player.setWhite(true);
+        }
+        if(white && blue && !red && !yellow && !black) {
+            setPuddleCol("cyan");
+            clearGate("cyan_gate");
+        }
+        if(black && !blue && !red && !yellow && !white) {
+            setPuddleCol("black");
+            player.setBlack(true);
+        }
+        if(black && red && !white && !blue && !yellow) {
+            setPuddleCol("brown");
+            clearGate("brown_gate");
+            player.setBrown(true);
+        }
+        if(yellow && !red && !blue && !white && !black) {
+            setPuddleCol("yellow");
+            player.setYellow(true);
+        }
+        if(yellow && red && !blue && !black && !white) {
+            setPuddleCol("orange");
+            clearGate("orange_gate");
+            player.setOrange(true);
+        }
+        if(secondWhite) {
+            setPuddleCol("white");
+            player.setSecondWhite(true);
+            clearGate("white_gate");
+        }
+        if(secondWhite && red) {
+            setPuddleCol("pink");
+            player.setPink(true);
+            clearGate("pink_gate");
+        }
+
         if(player.isColorChanged()) {
             changeColor(getPuddleCol());
             player.setupTextureRegion();
@@ -149,25 +236,37 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
             cell.setCell(23, 7, null);
             cell.setCell(23, 8, null);
             cell.setCell(23, 9, null);
-
         } else if(path.equals("red_gate")) {
             cell.setCell(16, 7, null);
             cell.setCell(16, 8, null);
             cell.setCell(16, 9, null);
             boolean redColorSet = true;
             player.setRed(redColorSet);
-
         } else if(path.equals("blue_gate")) {
             cell.setCell(8, 2, null);
             cell.setCell(8, 3, null);
             boolean blueColorSet = true;
             player.setBlue(blueColorSet);
         } else if(path.equals("red_gate_second")) {
-            cell.setCell(39, 7, null);
-            cell.setCell(39, 8, null);
+            cell.setCell(39, 6, null);
             cell.setCell(36, 10, null);
             cell.setCell(37, 10, null);
             player.setSecondRedColor(true);
+        } else if(path.equals("cyan_gate")) {
+            cell.setCell(36, 25, null);
+            cell.setCell(37, 25, null);
+            player.setCyan(true);
+        } else if(path.equals("brown_gate")) {
+            cell.setCell(36, 26, null);
+            cell.setCell(37, 26, null);
+        } else if(path.equals("orange_gate")) {
+            cell.setCell(36, 27, null);
+            cell.setCell(37, 27, null);
+        } else if(path.equals("white_gate")) {
+            cell.setCell(33, 47, null);
+            cell.setCell(34, 47, null);
+        } else if(path.equals("pink_gate")) {
+            cell.setCell(33, 39, null);
         }
     }
 
@@ -210,10 +309,28 @@ public class LevelTwo extends ApplicationAdapter implements Screen {
             player.setTexture(new Texture(Gdx.files.internal("sketch_ball_purple.png")));
         } else if (color.equals("green")) {
             player.setTexture(new Texture(Gdx.files.internal("sketch_ball_green.png")));
+        } else if (color.equals("white")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_white.png")));
+        } else if (color.equals("cyan")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_cyan.png")));
+        } else if (color.equals("brown")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_brown.png")));
+        } else if (color.equals("black")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_black.png")));
+        } else if (color.equals("yellow")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_yellow.png")));
+        } else if (color.equals("orange")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_orange.png")));
+        } else if(color.equals("pink")) {
+            player.setTexture(new Texture(Gdx.files.internal("sketch_ball_pink.png")));
         } else if (color.equals("null")) {
             player.setTexture(new Texture(Gdx.files.internal("sketch_ball.png")));
             redColorChanged = false;
             blueColorChanged = false;
+            secondRedColorChanged = false;
+            whiteColorChanged = false;
+            blackColorChanged = false;
+            yellowColorChanged = false;
         }
     }
 
